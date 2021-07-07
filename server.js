bb270ba094f8913964ccc1a4eebec40b59d181b4
Adin <1502842832@qq.com>
2021-07-07 12:43:55 +0800

    static init

---
+++ server.js
@@ -0,0 +1,56 @@
+// 静态服务器 
+var http = require('http')
+var fs = require('fs')
+var url = require('url')
+var port = process.argv[2]
+
+if (!port) {
+    console.log('请指定端口号，如\nnode server.js 8888 ')
+    process.exit(1)
+}
+
+var server = http.createServer(function (request, response) {
+    var parsedUrl = url.parse(request.url, true)
+    var pathWithQuery = request.url
+    var queryString = ''
+    if (pathWithQuery.indexOf('?') >= 0) { queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
+    var path = parsedUrl.pathname
+    var query = parsedUrl.query
+    var method = request.method
+
+    /******** init ************/
+
+    console.log('有个请求来了 路径（带查询参数）为：' + pathWithQuery)
+
+    response.statusCode = 200
+    const filePath = path === '/' ? '/index.html' : path
+    //默认首页
+    const index = filePath.lastIndexOf('.')
+    const suffix = filePath.substring(index)
+    const fileType = {
+        '.html': 'text/html',
+        '.css': 'text/css',
+        '.js': 'text/javascript',
+        '.json': 'text/json',
+        '.png': 'image/png',
+        '.jpg': 'image/jpeg'
+    }
+    response.setHeader('Content-Type',
+        `${fileType[suffix] || 'text/html'};charset=utf-8`)
+    let content
+    try {
+        content = fs.readFileSync(`./public${filePath}`)
+    }
+    catch (error) {
+        content = '文件不存在'
+        response.statusCode = 404
+    }
+    response.write(content)
+    response.end()
+
+    /******** 代码结束 ************/
+})
+
+server.listen(port)
+console.log('监听 ' + port + ' 成功\n请打开 http://localhost:' + port)
+
